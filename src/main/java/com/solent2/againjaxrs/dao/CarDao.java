194d95c43f13aa8f6d74023a63458a3ae5c424fa
Sahan Perera <sahanyuganthamail@gmail.com>
2020-10-11 16:59:32 +0530

    Log4j2 and JUnit tests added

--- src/main/java/com/solent2/againjaxrs/dao/CarDao.java
+++ src/main/java/com/solent2/againjaxrs/dao/CarDao.java
@@ -8,13 +8,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.mysql.cj.jdbc.Driver;
+import com.solent2.againjaxrs.CarResource;
 import com.solent2.againjaxrs.config.DatabaseConfiguration;
 import com.solent2.againjaxrs.model.CAR_TYPE;
 import com.solent2.againjaxrs.model.Car;
 
 public class CarDao {
 	
+	private Logger logger = LogManager.getLogger(CarDao.class);
 	
 	private List<Car> fakeData(){
 		List<Car> carList = new ArrayList<Car>();
@@ -27,6 +32,9 @@ public class CarDao {
 	}
 	
 	private List<Car> getDataFromDb() {
+		
+		logger.info("Car data fetched by user");
+		
 		try {
 			
 			Connection conn = DatabaseConfiguration.getInstance().getDbConnection();
@@ -78,6 +86,58 @@ public class CarDao {
 		}
 		
 		return car;
+	}
+	
+	public int addCar(Car car) {
+		try {
+				logger.info("Car data added by user");
+				Connection conn = DatabaseConfiguration.getInstance().getDbConnection();
+				if(conn != null) {
+				String sql = "INSERT INTO `tbl_car` (`model`, `brand`) VALUES (?,?)";
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				stmt.setString(1, car.getModel());
+				stmt.setString(2, car.getBrand());
+				
+				int rows = stmt.executeUpdate();
+				
+				return rows;
+			} else {
+				logger.error("Database connection is null");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.fatal("ERROR "+e.getMessage());
+		}
+		
+		return -1;
+	}
+	
+	public int updatCar(Car car) {
+		try {
+			logger.info("Car data updated by user");
+			Connection conn = DatabaseConfiguration.getInstance().getDbConnection();
+			if(conn != null) {
+				
+				String sql = "UPDATE `tbl_car` SET `model`=?, `brand`=? WHERE `id`=?";
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				stmt.setString(1, car.getModel());
+				stmt.setString(2, car.getBrand());
+				stmt.setInt(3, car.getId());
+				
+				int rows = stmt.executeUpdate();
+				
+				return rows;
+				
+			}else {
+				logger.error("Database connection is null");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.fatal("ERROR "+e.getMessage());
+		}
+		
+		return -1;
 	}
 
 }
---
+++ src/test/java/com/solent2/againjaxrs/tests/CarTest.java
@@ -0,0 +1,56 @@
+package com.solent2.againjaxrs.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.solent2.againjaxrs.dao.CarDao;
+import com.solent2.againjaxrs.model.Car;
+
+public class CarTest {
+	
+	
+	@Test
+	public void testFetchAll() {
+		int expected = 12;
+		
+		CarDao carDao = new CarDao();
+		List<Car> carList = carDao.getAll();
+		
+		int actual = carList.size();
+		
+		assertEquals(expected, actual);		
+		
+	}
+	
+//	@Test
+//	public void testaddCar() {
+//		int expected = 1;
+//		Car car = new Car();
+//		car.setModel("Jaguar X");
+//		car.setBrand("Jaguar");
+//		
+//		CarDao carDao = new CarDao();
+//		int actual = carDao.addCar(car);
+//		
+//		assertEquals(expected, actual);
+//		
+//	}
+	
+//	@Test
+//	public void testUpdateCar() {
+//		int expected = 1;
+//		Car car = new Car();
+//		car.setId(14);
+//		car.setModel("xyz");
+//		car.setBrand("JAGUAR");
+//		
+//		CarDao carDao = new CarDao();
+//		int actual = carDao.updatCar(car);
+//		
+//		assertEquals(expected, actual);
+//	}
+
+}
